3a27a18b8c26ac41d5eb9f3a271ecc3ebafaa1b1
Babak Ahari <babak.ahari@gmail.com>
2021-02-12 01:50:38 +0100

    view parameters
    Just simple code for figure out information in serial Port

---
+++ dht11-humidity_temp970228.ino.ino
@@ -0,0 +1,14 @@
+#include "DHT.h"
+DHT dht;
+void setup()
+{
+Serial.begin(9600);
+dht.setup(2); // data pin 2
+}
+void loop()
+{
+delay(dht.getMinimumSamplingPeriod());
+Serial.print("Humidity =");Serial.println(dht.getHumidity());
+Serial.print("\t");
+Serial.print("Temp =");Serial.println(dht.getTemperature());
+}
